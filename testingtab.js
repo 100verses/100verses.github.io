95f784ffd2351cf034a0f289f9f0ac4b9c52e588
Favian Ho <favianho@berkeley.edu>
2015-07-07 18:03:28 -0700

    added random verse tester

---
+++ testingtab.js
@@ -0,0 +1,279 @@
+
+var num = 1;
+
+var associativeArray = {};
+associativeArray[1] = "Genesis 1:1";
+associativeArray[2] = "Deuteronomy 6:6";
+associativeArray[3] = "Deuteronomy 6:7";
+associativeArray[4] = "Joshua 1:8";
+associativeArray[5] = "2 Chronicles 7:14";
+associativeArray[6] = "Psalm 4:8";
+associativeArray[7] = "Psalm 23:1";
+associativeArray[8] = "Psalm 23:2";
+associativeArray[9] = "Psalm 23:3";
+associativeArray[10] = "Psalm 23:4";
+associativeArray[11] = "Psalm 23:5";
+associativeArray[12] = "Psalm 23:6";
+associativeArray[13] = "Psalm 55:22";
+associativeArray[14] = "Psalm 56:3";
+associativeArray[15] = "Psalm 100:4";
+associativeArray[16] = "Psalm 100:5";
+associativeArray[17] = "Psalm 118:24";
+associativeArray[18] = "Psalm 119:11";
+associativeArray[19] = "Proverbs 1:7";
+associativeArray[20] = "Proverbs 3:5";
+associativeArray[21] = "Proverbs 3:6";
+associativeArray[22] = "Proverbs 15:1";
+associativeArray[23] = "Isaiah 9:6";
+associativeArray[24] = "Isaiah 26:3";
+associativeArray[25] = "Isaiah 53:5";
+associativeArray[26] = "Isaiah 53:6";
+associativeArray[27] = "Jeremiah 29:11";
+associativeArray[28] = "First";
+associativeArray[29] = "Second";
+associativeArray[30] = "Third";
+associativeArray[31] = "First";
+associativeArray[32] = "Second";
+associativeArray[33] = "Third";
+associativeArray[34] = "First";
+associativeArray[35] = "Second";
+associativeArray[36] = "Third";
+associativeArray[37] = "Third";
+associativeArray[38] = "First";
+associativeArray[39] = "Second";
+associativeArray[40] = "Third";
+associativeArray[41] = "First";
+associativeArray[42] = "Second";
+associativeArray[43] = "Third";
+associativeArray[44] = "First";
+associativeArray[45] = "Second";
+associativeArray[46] = "Third";
+associativeArray[47] = "Third";
+associativeArray[48] = "First";
+associativeArray[49] = "Second";
+associativeArray[50] = "Third";
+associativeArray[51] = "First";
+associativeArray[52] = "Second";
+associativeArray[53] = "Third";
+associativeArray[54] = "First";
+associativeArray[55] = "Second";
+associativeArray[56] = "Third";
+associativeArray[57] = "Third";
+associativeArray[58] = "First";
+associativeArray[59] = "Second";
+associativeArray[60] = "Third";
+associativeArray[61] = "First";
+associativeArray[62] = "Second";
+associativeArray[63] = "Third";
+associativeArray[64] = "First";
+associativeArray[65] = "Second";
+associativeArray[66] = "Third";
+associativeArray[67] = "Third";
+associativeArray[68] = "First";
+associativeArray[69] = "Second";
+associativeArray[70] = "Third";
+associativeArray[71] = "First";
+associativeArray[72] = "Second";
+associativeArray[73] = "Third";
+associativeArray[74] = "First";
+associativeArray[75] = "Second";
+associativeArray[76] = "Third";
+associativeArray[77] = "Third";
+associativeArray[78] = "First";
+associativeArray[79] = "Second";
+associativeArray[80] = "Third";
+associativeArray[81] = "First";
+associativeArray[82] = "Second";
+associativeArray[83] = "Third";
+associativeArray[84] = "First";
+associativeArray[85] = "Second";
+associativeArray[86] = "Third";
+associativeArray[87] = "Third";
+associativeArray[88] = "First";
+associativeArray[89] = "Second";
+associativeArray[90] = "Third";
+associativeArray[91] = "First";
+associativeArray[92] = "Second";
+associativeArray[93] = "Third";
+associativeArray[94] = "First";
+associativeArray[95] = "Second";
+associativeArray[96] = "Third";
+associativeArray[97] = "Third";
+associativeArray[98] = "First";
+associativeArray[99] = "Second";
+associativeArray[100] = "Third";
+
+var associativeArray2 = {};
+associativeArray2[1] = "In the beginning, God created the heavens and the earth.";
+associativeArray2[2] = "And these words that I command you today shall be on your heart.";
+associativeArray2[3] = "You shall teach them diligently to your children, and shall talk of them when you sit in your house, and when you walk by the way, and when you lie down, and when you rise.";
+associativeArray2[4] = "The Book of the Law shall not depart from your mouth, but you shall meditate on it day and night, so that you may be careful to do according to all that is written in it. For then you will make your way prosperous, and then you will have good success.";
+associativeArray2[5] = "if my people who are called by name humble themselves, and pray and seek my face and turn from their wicked ways, then I will hear from heaven and will forgive their sin and heal their land.";
+associativeArray2[6] = "In peace I will both lie down and sleep; for you alone, O Lord, make me dwell in safety.";
+associativeArray2[7] = "The Lord is my shepherd; I shall not want.";
+associativeArray2[8] = "He makes me lie down in green pastures. He leads me beside still waters.";
+associativeArray2[9] = "He restores my soul. He leads me in paths of righteousness for his name's sake.";
+associativeArray2[10] = "Even though I walk through the valley of the shadow of death, I will fear no evil, for you are with me; your rod and your staff, they comfort me.";
+associativeArray2[11] = "You prepare a table before me in the presence of my enemies; you anoint my head with oil; my cup overflows.";
+associativeArray2[12] = "Surely goodness and mercy shall follow me all the days of my life, and I shall dwell in the house of the Lord forever";
+associativeArray2[13] = "Cast your burden on the Lord, and he will sustain you; he will never permit the righteous to be moved.";
+associativeArray2[14] = "When I am afraid, I put my trust in you.";
+associativeArray2[15] = "Enter his gates with thanksgiving, and his courts with praise! Give thanks to him; bless his name!";
+associativeArray2[16] = "For the Lord is good; his steadfast love endures forever, and his faithfulness to all generations.";
+associativeArray2[17] = "This is the day that the Lord has made; let us rejoice and be glad in it.";
+associativeArray2[18] = "I have stored up your word in my heart, that I might not sin against you.";
+associativeArray2[19] = "The fear of the Lord is the beginning of knowledge; fools despise wisdom and instruction.";
+associativeArray2[20] = "I lay down and slept; I woke again, for the Lord sustained me.";
+associativeArray2[21] = "I will not be afraid of many thousands of people who have set themselves against me all around.";
+associativeArray2[22] = "A soft answer turns away wrath, but a harsh word stirs up anger.";
+associativeArray2[23] = "For to us a child is born, to us a son is given; and the government shall be upon his shoulder, and his name shall be called Wonderful Counselor, Mighty God, Everlasting Father, Prince of Peace.";
+associativeArray2[24] = "You keep him in perfect peace whose mind is stayed on you, because he trusts in you.";
+associativeArray2[25] = "But he was pierced for our transgressions; he was crushed for ouy iniquities; upon him was the chastisement that brought us peace, and with his wounds we are healed.";
+associativeArray2[26] = "All we like sheep have gone astray; we have turned -every one- to his own way; and the Lord has laid on him the iniquity of us all.";
+associativeArray2[27] = "For I know the plans I have for you, declares the Lord, plans for welfare and not for evil, to give you a future and a hope.";
+associativeArray2[28] = "First";
+associativeArray2[29] = "Second";
+associativeArray2[30] = "Third";
+associativeArray2[31] = "First";
+associativeArray2[32] = "Second";
+associativeArray2[33] = "Third";
+associativeArray2[34] = "First";
+associativeArray2[35] = "Second";
+associativeArray2[36] = "Third";
+associativeArray2[37] = "Third";
+associativeArray2[38] = "First";
+associativeArray2[39] = "Second";
+associativeArray2[40] = "Third";
+associativeArray2[41] = "First";
+associativeArray2[42] = "Second";
+associativeArray2[43] = "Third";
+associativeArray2[44] = "First";
+associativeArray2[45] = "Second";
+associativeArray2[46] = "Third";
+associativeArray2[47] = "Third";
+associativeArray2[48] = "First";
+associativeArray2[49] = "Second";
+associativeArray2[50] = "Third";
+associativeArray2[51] = "First";
+associativeArray2[52] = "Second";
+associativeArray2[53] = "Third";
+associativeArray2[54] = "First";
+associativeArray2[55] = "Second";
+associativeArray2[56] = "Third";
+associativeArray2[57] = "Third";
+associativeArray2[58] = "First";
+associativeArray2[59] = "Second";
+associativeArray2[60] = "Third";
+associativeArray2[61] = "First";
+associativeArray2[62] = "Second";
+associativeArray2[63] = "Third";
+associativeArray2[64] = "First";
+associativeArray2[65] = "Second";
+associativeArray2[66] = "Third";
+associativeArray2[67] = "Third";
+associativeArray2[68] = "First";
+associativeArray2[69] = "Second";
+associativeArray2[70] = "Third";
+associativeArray2[71] = "First";
+associativeArray2[72] = "Second";
+associativeArray2[73] = "Third";
+associativeArray2[74] = "First";
+associativeArray2[75] = "Second";
+associativeArray2[76] = "Third";
+associativeArray2[77] = "Third";
+associativeArray2[78] = "First";
+associativeArray2[79] = "Second";
+associativeArray2[80] = "Third";
+associativeArray2[81] = "First";
+associativeArray2[82] = "Second";
+associativeArray2[83] = "Third";
+associativeArray2[84] = "First";
+associativeArray2[85] = "Second";
+associativeArray2[86] = "Third";
+associativeArray2[87] = "Third";
+associativeArray2[88] = "First";
+associativeArray2[89] = "Second";
+associativeArray2[90] = "Third";
+associativeArray2[91] = "First";
+associativeArray2[92] = "Second";
+associativeArray2[93] = "Third";
+associativeArray2[94] = "First";
+associativeArray2[95] = "Second";
+associativeArray2[96] = "Third";
+associativeArray2[97] = "Third";
+associativeArray2[98] = "First";
+associativeArray2[99] = "Second";
+associativeArray2[100] = "Third";
+
+
+
+var submit = function (){
+	var input = document.getElementById("userInput").value;
+	var string1mid = input.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+	var string1 = string1mid.replace(/\s{2,}/g," ");
+	var string2beg = "For I am not ashamed of the gospel, for it is the power of God for salvation to everyone who believes, to the Jew first and also to the Greek."
+	var string2mid = string2beg.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+	var string2 = string2mid.replace(/\s{2,}/g," ");
+	var areEqual = string1.toUpperCase() === string2.toUpperCase();
+	if (areEqual){
+		alert("CORRECT! :D")
+	}
+	else{
+		alert("Sorry, you are wrong :(")
+	}
+}
+
+var submit2 = function (){
+	var string1 = document.getElementById("userInput2").value;
+	var string2 = "Psalm 100:5"
+	var areEqual = string1.toUpperCase() === string2.toUpperCase();
+	if (areEqual){
+		alert("CORRECT! :D")
+	}
+	else{
+		alert("Sorry, you are wrong :(")
+	}
+}
+
+var submit3 = function (){
+	var input = document.getElementById("userInput3").value;
+	var string1mid = input.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+	var string1 = string1mid.replace(/\s{2,}/g," ");
+	var string2beg = associativeArray2[num]
+	var string2mid = string2beg.replace(/[\.,-\/#!$%\^&\*;:{}=\-_`~()]/g,"");
+	var string2 = string2mid.replace(/\s{2,}/g," ");
+	var areEqual = string1.toUpperCase() === string2.toUpperCase();
+	if (areEqual){
+		alert("CORRECT! :D")
+	}
+	else{
+		alert("Sorry, you are wrong :(")
+	}
+}
+
+var showhide = function (){
+	if (document.getElementById('randverse').innerHTML == ""){
+		document.getElementById('randverse').innerHTML = associativeArray2[num];
+	}
+	else{
+		document.getElementById('randverse').innerHTML = "";
+	}
+}
+
+var skip = function (){
+	var newRand = Math.ceil(100*Math.random())
+	while (newRand>27){
+		newRand = Math.ceil(100*Math.random())
+	}
+	num = newRand
+	document.getElementById('randref').innerHTML = associativeArray[num];
+	document.getElementById('randverse').innerHTML = "";
+}
+
+var donothing = function(){
+
+}
+
+
+
+
